f789d44c6202119312949eaf508a69022c5efc2e
GustavoNoronha <gustavonoronhaprog@hotmail.com>
2020-12-22 14:48:21 -0300

    Post e Get Controller Pedidos

--- src/routes.js
+++ src/routes.js
@@ -1,10 +1,15 @@
 const express = require('express');
 
+const PedidoControler = require('./controllers/PedidoController')
+
+
 const routes = express.Router();
 
 routes.get('/', (req,res) =>{
     return res.json({hello: 'World'})
 });
-
+// pedidos 
+routes.post('/pedidos', PedidoControler.store );
+routes.get('/pedidos', PedidoControler.index );
 
 module.exports = routes;
