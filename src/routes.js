d290aa77b6366bbeed8868d4e0eeb327525defc1
GustavoNoronha <gustavonoronhaprog@hotmail.com>
2020-12-22 15:58:08 -0300

    Todas as Tabelas Rotas e Migrations

--- src/routes.js
+++ src/routes.js
@@ -1,6 +1,15 @@
 const express = require('express');
 
-const PedidoControler = require('./controllers/PedidoController')
+const AdministradorController = require('./controllers/AdministradorController');
+const CartoesController = require('./controllers/CartoesController');
+const CategoriaController = require('./controllers/CategoriaController');
+const EnderecoController = require('./controllers/EnderecoController');
+const OfertasController = require('./controllers/OfertasController');
+const PedidoControler = require('./controllers/PedidoController');
+const PerfilController = require('./controllers/PerfilController');
+const PromocoesController = require('./controllers/PromocoesController');
+const RestauranteController = require('./controllers/RestauranteController');
+const SugestoesController = require('./controllers/SugestoesController');
 
 
 const routes = express.Router();
@@ -11,5 +20,41 @@ routes.get('/', (req,res) =>{
 // pedidos 
 routes.post('/pedidos', PedidoControler.store );
 routes.get('/pedidos', PedidoControler.index );
+
+// Administrador
+routes.post('/administrador', AdministradorController.store );
+routes.get('/administrador', AdministradorController.index );
+
+//Cartoes
+routes.post('/cartoes', CartoesController.store );
+routes.get('/cartoes', CartoesController.index );
+
+// Categoria
+routes.post('/categoria', CategoriaController.store );
+routes.get('/categoria', CategoriaController.index );
+
+// Endereco
+routes.post('/endereco', EnderecoController.store );
+routes.get('/endereco', EnderecoController.index );
+
+// Ofertas
+routes.post('/ofertas', OfertasController.store );
+routes.get('/ofertas', OfertasController.index );
+
+// Perfil
+routes.post('/perfil', PerfilController.store );
+routes.get('/perfil', PerfilController.index );
+
+// Promocoes
+routes.post('/promocoes', PromocoesController.store );
+routes.get('/promocoes', PromocoesController.index );
+
+// Restaurantes
+routes.post('/restaurante', RestauranteController.store );
+routes.get('/restaurante', RestauranteController.index );
+
+// Sugestoes
+routes.post('/sugestoes', SugestoesController.store );
+routes.get('/sugestoes', SugestoesController.index );
 
 module.exports = routes;
